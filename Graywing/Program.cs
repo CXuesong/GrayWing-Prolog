00621beace8f9cedc3595b257a5cb9ce35f04653
forest93 <forest93@163.com>
2017-08-28 20:26:23 +0800

    添加项目文件。

---
+++ Graywing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Graywing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
