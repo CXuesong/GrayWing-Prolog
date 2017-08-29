8c739976afb742e204e2226ee0e6d52595bb52b7
forest93 <forest93@163.com>
2017-08-29 12:34:39 +0800

    Add sibling rules. Fix incorrect rules for parent.

--- Prolog/rules.pl
+++ Prolog/rules.pl
@@ -12,6 +12,18 @@ grandchild(X, Y) :- child(X, PARENT), child(PARENT, Y).
 grandson(X, Y) :- male(X), grandchild(X, Y).
 granddaughter(X, Y) :- female(X), grandchild(X, Y).
 
-grandparent(X, Y) :- grandson(Y, X).
+grandparent(X, Y) :- grandchild(Y, X).
 grandfather(X, Y) :- male(X), grandparent(X, Y).
 grandmother(X, Y) :- female(X), grandparent(X, Y).
+
+sibling(X, Y) :- child(X, FATHER, MOTHER), child(Y, FATHER, MOTHER), X \== Y.
+brother(X, Y) :- male(X), sibling(X, Y).
+sister(X, Y) :- female(X), sibling(X, Y).
+
+halfsibling(X, Y) :- (
+        (child(X, FATHER, MOTHER1), child(Y, FATHER, MOTHER2), MOTHER1 \== MOTHER2);
+        (child(X, FATHER1, MOTHER), child(Y, FATHER2, MOTHER), FATHER1 \== FATHER2)
+    ), X \== Y.
+halfbrother(X, Y) :- male(X), halfsibling(X, Y).
+halfsister(X, Y) :- female(X), halfsibling(X, Y).
+
